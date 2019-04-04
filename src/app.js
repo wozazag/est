1eccc656317c4d78d8edc153b538072cea8f57bc
wozazag <wozazag@gmail.com>
2019-04-04 10:39:09 +0900

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+
+import "./styles.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-log">
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="http://reactjs.org"
+            target="_blank"
+            rel="dddddd"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
